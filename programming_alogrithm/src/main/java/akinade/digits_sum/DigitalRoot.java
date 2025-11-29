308aafc6d2c6f7d6355cd04d46441c61a1a47c26
akinade <adebusoyeteeman@gmail.com>
2025-11-29 18:29:42 +0100

    DigitalRoot Implementation

--- programming_alogrithm/src/main/java/akinade/digits_sum/DigitalRoot.java
+++ programming_alogrithm/src/main/java/akinade/digits_sum/DigitalRoot.java
@@ -1,4 +1,24 @@
 package akinade.digits_sum;
 
 public class DigitalRoot {
+    public static int sumDigits(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
+        int first = s.charAt(0) - '0';  //
+        return first + sumDigits(s.substring(1));
+    }
+
+
+    public static int digitalRoot(String s) {
+        int sum = sumDigits(s);
+
+        // Base case: single digit  // 23
+        if (sum < 10) {
+            System.out.println("Next sum: " + sum);
+            return sum;
+        }
+
+        return digitalRoot(String.valueOf(sum));
+    }
+
 }
