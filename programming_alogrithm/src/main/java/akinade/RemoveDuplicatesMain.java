2957c3741cb676469fd2c1dddd24f9f2cc5b8ee5
akinade <adebusoyeteeman@gmail.com>
2025-11-29 18:31:04 +0100

    RemoveDuplicate runtime

--- programming_alogrithm/src/main/java/akinade/RemoveDuplicatesMain.java
+++ programming_alogrithm/src/main/java/akinade/RemoveDuplicatesMain.java
@@ -1,4 +1,25 @@
 package akinade;
 
+import static akinade.remove_duplicates.RemoveDuplicates.removeDuplicates;
+
 public class RemoveDuplicatesMain {
+    public static void main(String[] args) {
+        int[][] input = {
+                {1, 3, 1, 5, 3, 4, 4, 3, 5},
+                {1, 2, 1, 1, 1, 1, 1}
+        };
+
+        int[][] output = removeDuplicates(input);
+
+
+        for (int[] row : output) {
+            System.out.print("{ ");
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println("}");
+        }
+
+    }
+
 }
