63ec025a37fa0549a22e92e367617275c468f74d
Teeman <adebusoyeteeman@gmai.com>
2025-12-01 19:48:17 +0100

    student service repositories

---
+++ student-service/src/main/java/akinade/domain/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package akinade.domain.repository;
+
+import akinade.domain.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+}
