9fa2a8a51173f0be16f57b7895a0eb77001c20d8
Teeman <adebusoyeteeman@gmai.com>
2025-12-01 20:07:50 +0100

    report-service jre

---
+++ report-service/src/main/java/akinade/ReportServiceApplication.java
@@ -0,0 +1,13 @@
+package akinade;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ReportServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportServiceApplication.class, args);
+    }
+}
