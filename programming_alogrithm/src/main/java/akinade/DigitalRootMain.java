8fe1656d4e2cfe20ccb5be6b3013a2639d97986d
Teeman <adebusoyeteeman@gmai.com>
2025-12-10 03:59:05 +0100

    update DigitalRootMain

--- programming_alogrithm/src/main/java/akinade/DigitalRootMain.java
+++ programming_alogrithm/src/main/java/akinade/DigitalRootMain.java
@@ -1,4 +1,21 @@
 package akinade;
+import java.util.Scanner;
 
-public class DigitRootMain {
+import static akinade.digits_sum.DigitalRoot.digitalRoot;
+import static akinade.digits_sum.DigitalRoot.sumDigits;
+
+public class DigitalRootMain {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input digits (e.g: 1234445): ");
+        String input = sc.nextLine(); //
+
+        System.out.println();
+
+        int firstSum = sumDigits(input);
+        System.out.println("First sum: = " + firstSum);
+        int result = digitalRoot(input);
+        System.out.println("Digital root = " + result);}
+
 }
